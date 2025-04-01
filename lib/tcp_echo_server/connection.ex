49859650946990be59c1dcc7f0ff0aee87fcab9b
Carlo Gilmar <carlogilmar12@gmail.com>
2025-04-01 01:02:06 -0600

    Setup observer

--- lib/tcp_echo_server/connection.ex
+++ lib/tcp_echo_server/connection.ex
@@ -13,7 +13,7 @@ defmodule TCPEchoServer.Connection do
 
   @impl true
   def init(socket) do
-    Logger.info("Step 4 <Connection Process>: initializing new connection.")
+    Logger.info("Step 4 <Connection Process>: initializing new connection managed by process #{inspect(self())}.")
     state = %__MODULE__{socket: socket}
     {:ok, state}
   end
@@ -21,11 +21,10 @@ defmodule TCPEchoServer.Connection do
   @impl true
   def handle_info(message, state)
 
-  def handle_info(
-        {:tcp, socket, data},
-        %__MODULE__{socket: socket} = state
-      ) do
-    IO.puts("Connection Handle Info!!! I'm here!")
+	def handle_info({:tcp, socket, data},
+		%__MODULE__{socket: socket} = state
+	) do
+    IO.puts "Connection Handle Info!!! I'm here!"
     state = update_in(state.buffer, &(&1 <> data))
     state = handle_new_data(state)
     {:noreply, state}
