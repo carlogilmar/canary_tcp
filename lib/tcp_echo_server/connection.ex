ff4ea08317d1f274013a565b9a03d628175f7946
Carlo Gilmar <carlogilmar12@gmail.com>
2025-04-01 12:24:39 -0600

    Adding logger info

--- lib/tcp_echo_server/connection.ex
+++ lib/tcp_echo_server/connection.ex
@@ -13,7 +13,10 @@ defmodule TCPEchoServer.Connection do
 
   @impl true
   def init(socket) do
-    Logger.info("Step 4 <Connection Process>: initializing new connection managed by process #{inspect(self())}.")
+    Logger.info(
+      "Step 4 <Connection Process>: initializing new connection managed by process #{inspect(self())}."
+    )
+
     state = %__MODULE__{socket: socket}
     {:ok, state}
   end
@@ -21,16 +24,18 @@ defmodule TCPEchoServer.Connection do
   @impl true
   def handle_info(message, state)
 
-	def handle_info({:tcp, socket, data},
-		%__MODULE__{socket: socket} = state
-	) do
-    IO.puts "Connection Handle Info!!! I'm here!"
+  def handle_info(
+        {:tcp, socket, data},
+        %__MODULE__{socket: socket} = state
+      ) do
+    IO.puts("Connection Handle Info!!! I'm here!")
     state = update_in(state.buffer, &(&1 <> data))
     state = handle_new_data(state)
     {:noreply, state}
   end
 
   def handle_info({:tcp_closed, socket}, %__MODULE__{socket: socket} = state) do
+    Logger.info("TCP Closed for process #{inspect(self())}")
     {:stop, :normal, state}
   end
 
